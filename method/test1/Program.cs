933cfb2d1c0f2706347382d21e8a25a047d8da3f
AleksandrKuznetsov2022 <jivin@211.ru>
2022-04-07 16:38:34 +0300

    test

---
+++ method/test1/Program.cs
@@ -0,0 +1,20 @@
+﻿static string ReversWord(string splitPhrase)
+{
+    string[] array = SplitToWords(splitPhrase);
+    string ResaltString = string.Empty;
+    for (int i = array.Length - 1; i >= 0; i--)
+    {
+        ResaltString += " " + array[i];
+    }
+    Console.WriteLine(ResaltString);
+    return ResaltString;
+}
+static string[] SplitToWords(string inputPhrase)
+{
+    string [] arrayWords = inputPhrase.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    return arrayWords;
+}
+Console.WriteLine("Введите слова разделенные пробелом");
+string inputPhrase = Console.ReadLine();
+ReversWord(inputPhrase);
+Console.WriteLine(inputPhrase);
